b8a149eac90d95992e93056c38612cb952b0f7c3
gq <guanquntan@gmail.com>
2024-02-02 01:35:10 +0800

    fix minor bug

---
+++ src/app/api/organisations/route.ts
@@ -0,0 +1,73 @@
+import { Prisma } from '@prisma/client'
+import { NextResponse, NextRequest } from 'next/server';
+import prisma from '../../../lib/prisma'
+
+const sampleJSON = {
+    "email": "BAH@mail.com",
+    "password": "bigassheart",
+    "name": "Big At Heart",
+    "mobileNumber": "66666666",
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const requestBody = await request.json();
+        const { email, password, name, mobileNumber } = requestBody;
+
+        let user: Prisma.UserCreateInput = {
+            email: email,
+            hashPassword: password
+        };
+
+        const createUser = await prisma.user.create({ data: user });
+
+        let organisation: Prisma.OrganisationCreateInput = {
+            name: name,
+            mobileNumber: mobileNumber,
+            user: {
+                connect: createUser
+            }
+        }
+        const organisationData = await prisma.organisation.create({ data: organisation });
+
+        return NextResponse.json(organisationData, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    }
+}
+
+export async function PATCH(request: NextRequest) {
+    try {
+        const requestBody = await request.json();
+        const { id, name, mobileNumber } = requestBody;
+        const org: Prisma.OrganisationUpdateInput = {
+            name: name,
+            mobileNumber: mobileNumber
+        };
+
+        const updateEvent = await prisma.organisation.update({
+            where: {
+                id: id,
+            },
+            data: org
+        });
+        return NextResponse.json(updateEvent, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    }
+}
+
+export async function DELETE(request: NextRequest) {
+    try {
+        const requestBody = await request.json()
+        const { id } = requestBody;
+        const deleteEvent = await prisma.organisation.delete({
+            where: {
+                id: id,
+            },
+        });
+        return NextResponse.json(deleteEvent, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    }
+}
--- src/app/api/volunteers/route.ts
+++ src/app/api/volunteers/route.ts
@@ -1,12 +1,21 @@
-import { Prisma } from '@prisma/client'
 import { NextResponse, NextRequest } from 'next/server';
 import prisma from '../../../lib/prisma'
+import { Prisma } from '@prisma/client';
+import { add } from 'date-fns';
 
+const sampleJSON = {
+    "email": "mono@mail.com",
+    "password": "monomono",
+    "name": "mono.",
+    "mobileNumber": "12345678",
+    "bday": "2003-11-02T17:29:49.023Z", // use toJSON
+    "address": "", // OPTIONAL!
+    "availability": "WEEKEND"
+}
 export async function POST(request: NextRequest) {
     try {
         const requestBody = await request.json();
-        const { email, password, name, mobileNumber } = requestBody;
-
+        const { email, password, name, mobileNumber, bday, availability, address } = requestBody;
         let user: Prisma.UserCreateInput = {
             email: email,
             hashPassword: password
@@ -14,17 +23,23 @@ export async function POST(request: NextRequest) {
 
         const createUser = await prisma.user.create({ data: user });
 
-        let organisation: Prisma.OrganisationCreateInput = {
+        let volunteer: Prisma.VolunteerCreateInput = {
             name: name,
             mobileNumber: mobileNumber,
+            bday: bday,
+            availability: availability,
+            address: address,
             user: {
-                connect: createUser
+                connect: {
+                    id: createUser.id
+                }
             }
         }
-        const organisationData = await prisma.organisation.create({ data: organisation });
+        const data = await prisma.volunteer.create({ data: volunteer });
 
-        return NextResponse.json(organisationData, { status: 201 });
+        return NextResponse.json(data, { status: 201 });
     } catch (error) {
+        console.log(error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
     }
 }
@@ -32,17 +47,20 @@ export async function POST(request: NextRequest) {
 export async function PATCH(request: NextRequest) {
     try {
         const requestBody = await request.json();
-        const { id, name, mobileNumber } = requestBody;
-        const org: Prisma.OrganisationUpdateInput = {
+        const { id, name, mobileNumber, bday, availability, address } = requestBody;
+        const inputData: Prisma.VolunteerUpdateInput = {
             name: name,
-            mobileNumber: mobileNumber
+            mobileNumber: mobileNumber,
+            bday: bday,
+            availability: availability,
+            address: address
         };
 
-        const updateEvent = await prisma.organisation.update({
+        const updateEvent = await prisma.volunteer.update({
             where: {
                 id: id,
             },
-            data: org
+            data: inputData
         });
         return NextResponse.json(updateEvent, { status: 201 });
     } catch (error) {
@@ -54,13 +72,14 @@ export async function DELETE(request: NextRequest) {
     try {
         const requestBody = await request.json()
         const { id } = requestBody;
-        const deleteEvent = await prisma.organisation.delete({
+        const deleteEvent = await prisma.volunteer.delete({
             where: {
                 id: id,
             },
         });
         return NextResponse.json(deleteEvent, { status: 201 });
     } catch (error) {
+        console.log(error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
     }
 }
