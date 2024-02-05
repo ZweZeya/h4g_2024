bcdab15e55e5b077aa81803dc9e55001fe8e3cca
zwe <zwezeya02@gmail.com>
2024-02-05 23:16:34 +0800

    add db:ui script

--- src/app/(main)/calendar/page.tsx
+++ src/app/(main)/calendar/page.tsx
@@ -2,20 +2,17 @@ import prisma from '../../../lib/prisma'
 import BigCalendar from "@/components/Calendar/BigCalendar";
 import UpcomingEventsList from "@/components/Calendar/UpcomingEventsList";
 import { Event } from '@/types/EventTypes';
-import events from "@/components/Calendar/events";
 
 const CalendarPage = async () => {
-    const eventsData = await prisma.event.findMany();
-    const events: Event[] = [];
-    for (var e of eventsData) {
-        events.push({
+    const events: Event[] = (await prisma.event.findMany()).map((e: any) => {
+        return {
             id: e.id,
             title: e.name,
             start: e.startDate.toISOString().substr(0, 10),
             end: e.endDate.toISOString().substr(0, 10),
             location: e.location
-        });
-    }
+        }
+    });
 
     return (
         <div className="flex flex-col gap-3">
