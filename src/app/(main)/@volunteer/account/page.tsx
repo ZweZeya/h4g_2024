4e0989338ed5a6d6a1eadac3739ecbd4ad333ed2
gq <guanquntan@gmail.com>
2024-02-08 00:58:39 +0800

    wip account page

--- src/app/(main)/@volunteer/account/page.tsx
+++ src/app/(main)/@volunteer/account/page.tsx
@@ -1,9 +1,22 @@
 const AccountPage = () => {
     return (
-        <div>
-            <h1>Account Page</h1>
+        <div className="grid grid-cols-4 gap-14">
+            <div className="col-span-1">
+
+            </div>
+            <div className="col-span-2">
+                <p className="font-sans font-bold text-3xl">My Name</p>
+                <p className="font-sans font-bold text-lg">Bio</p>
+                <p>Age: 26</p>
+                <p>Nationality: Singaporean</p>
+                <p>Availability: Weekends</p>
+            </div>
+            <div className="col-span-1">
+
+            </div>
+
         </div>
     );
 };
-  
+
 export default AccountPage;
