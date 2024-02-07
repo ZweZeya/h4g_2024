08096dae903a350058e1ba97c0558731c3dfae95
Xueting <xueting1966@gmail.com>
2024-02-08 00:35:10 +0800

    button blue

--- src/components/Dashboard/SmallEvents.tsx
+++ src/components/Dashboard/SmallEvents.tsx
@@ -8,17 +8,19 @@ const SmallEvents = ({ event }: { event: EventCardProps }) => {
 
     return (
         <div className={"border border-[#e8e8e8] border-l-8 rounded-md my-4 px-2 py-3 flex gap-32 items-center"}>
-            <div className="flex flex-col items-center">
-            </div>
-            <div className="text-h3-s text-left tracking-wide">
+            <div className="flex flex-row gap-40 items-center">
+            <div className="flex text-h3-s text-left">
                 <p>{event.name}</p>
             </div>
-            <div className="text-body-s tracking-wide">
+            <div className="flex flex-col text-body-s text-left">
                 <p> {event.location} </p>
                 <p>{date} {month}</p>
             </div>
-            <Button>Enroll</Button>
+            <div className="flex justify-end">
+            <Button variant="mine">Enroll</Button>
+            </div>
         </div>
+    </div>
     );
 };
 
