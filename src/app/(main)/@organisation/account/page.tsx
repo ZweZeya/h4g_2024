cfef3f5c6b5fba8b23f28f6378270d7ab80b518a
gq <guanquntan@gmail.com>
2024-02-08 00:31:37 +0800

    exploreventcard

---
+++ src/app/(main)/@organisation/account/page.tsx
@@ -0,0 +1,9 @@
+const AccountPage = () => {
+    return (
+        <div>
+            <h1>Account Page</h1>
+        </div>
+    );
+};
+
+export default AccountPage;
--- src/app/(main)/@organisation/explore/page.tsx
+++ src/app/(main)/@organisation/explore/page.tsx
@@ -1,12 +1,28 @@
 import Header from '@/components/Layout/Header';
 
+import ExploreEventCard from './../../../../components/Event/ExploreEventCard'
+import prisma from './../../../../lib/prisma'
 
-const ExplorePage = async () => {
+const EventsPage = async () => {
+    const events = await prisma.event.findMany({
+        where: {
+            organisationId: 1 // TBC
+        }
+    });
     return (
         <div>
-            <Header>Organisation Explore Page</Header>
-        </div>
-    );
+            {events.map(e =>
+                <ExploreEventCard
+                    id={e.id}
+                    name={e.name}
+                    location={e.location}
+                    start={e.start}
+                    end={e.end}
+                    organisation="AAAA" //TBC
+                />
+            )}
+        </div >
+    )
 };
 
-export default ExplorePage;
+export default EventsPage;
