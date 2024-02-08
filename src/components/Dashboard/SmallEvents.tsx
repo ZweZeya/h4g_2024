8333afd500f29d4f758b9482d63de7b94722ee5c
gq <guanquntan@gmail.com>
2024-02-08 23:08:28 +0800

    align events dashboard

--- src/components/Dashboard/SmallEvents.tsx
+++ src/components/Dashboard/SmallEvents.tsx
@@ -7,20 +7,20 @@ const SmallEvents = ({ event }: { event: EventCardProps }) => {
     const date = startDateArr[2];
 
     return (
-        <div className={"border border-[#e8e8e8] border-l-8 rounded-md my-4 px-2 py-3 flex gap-32 items-center"}>
-            <div className="flex flex-row gap-40 items-center">
-            <div className="flex text-h3-s text-left">
-                <p>{event.name}</p>
-            </div>
-            <div className="flex flex-col text-body-s text-left">
-                <p> {event.location} </p>
-                <p>{date} {month}</p>
-            </div>
-            <div className="flex justify-end">
-            <Button variant="mine">Enroll</Button>
+        <div className={"border border-[#e8e8e8] border-l-8 rounded-md my-4 px-2 py-3 gap-32"}>
+            <div className="grid grid-cols-4 flex-row gap-40 items-center">
+                <div className="col-span-1">
+                    <p>{event.name}</p>
+                </div>
+                <div className="col-span-2">
+                    <p> {event.location} </p>
+                    <p>{date} {month}</p>
+                </div>
+                <div className="col-span-1 flex-row justify-end">
+                    <Button variant="mine">Enroll</Button>
+                </div>
             </div>
         </div>
-    </div>
     );
 };
 
