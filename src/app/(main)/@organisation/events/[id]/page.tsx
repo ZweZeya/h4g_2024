d2edd558d5aab3e209de90e49860edfd6e1ea6af
chongsun2002 <119829137+chongsun2002@users.noreply.github.com>
2024-02-10 22:41:05 +0800

    semi-completed enrollment dashboard

--- src/app/(main)/@organisation/events/[id]/page.tsx
+++ src/app/(main)/@organisation/events/[id]/page.tsx
@@ -1,18 +1,86 @@
 import prisma from './../../../../../lib/prisma'
 import EnrollmentAcceptedList from '../../../../../components/Event/EnrollmentAcceptedList'
+import { VolunteerEnrollment } from '../../../../../components/Event/EnrollmentAcceptedList';
+import { PendingColumns } from '../../../../../components/Event/OrganisationEventDisplay/PendingColumns';
+import { EnrolledColumns } from '@/components/Event/OrganisationEventDisplay/EnrolledColumns';
+import { DataTableCompleted } from '../../../../../components/Event/OrganisationEventDisplay/DataTableCompleted';
+import { DataTablePending } from '../../../../../components/Event/OrganisationEventDisplay/DataTablePending'
 
 const EventEnrollmentPage = async ({ params }: { params: { id: string } }) => {
-    const volunteersAccepted = await prisma.enrollment.findMany({
+    const getEnrolledVolunteers = await prisma.enrollment.findMany({
         where: {
-          eventId: Number(params.id)
+            AND: [
+                {eventId: Number(params.id)},
+                {
+                    OR: [
+                        { status: 'ASSIGNED' },
+                        { status: 'COMPLETED' }
+                    ]
+                }
+            ]
         },
         select: {
-          volunteer: true
+          volunteer: {
+            select: {
+                id: true,
+                name: true,
+                mobileNumber: true,
+                availability: true
+            }
+          },
+          status: true
         }
     });
+
+    const getPendingVolunteers = await prisma.enrollment.findMany({
+        where: {
+            AND: [
+                {eventId: Number(params.id)},
+                { status: 'PENDING' }
+            ]
+        },
+        select: {
+          volunteer: {
+            select: {
+                id: true,
+                name: true,
+                mobileNumber: true,
+                availability: true
+            }
+          },
+          status: true
+        }
+    });
+
+    const enrolledVolunteers = getEnrolledVolunteers.map(v => {
+        const volunteer: VolunteerEnrollment = {
+            id: v.volunteer.id,
+            name: v.volunteer.name,
+            mobileNumber: v.volunteer.mobileNumber,
+            availability: v.volunteer.availability,
+            enroll_status: v.status
+        }
+        return volunteer;
+    })
+
+    const pendingVolunteers = getPendingVolunteers.map(v => {
+        const volunteer: VolunteerEnrollment = {
+            id: v.volunteer.id,
+            name: v.volunteer.name,
+            mobileNumber: v.volunteer.mobileNumber,
+            availability: v.volunteer.availability,
+            enroll_status: v.status
+        }
+        return volunteer;
+    })
+
+
     return (
         <div>
-            <EnrollmentAcceptedList volunteers={volunteersAccepted}></EnrollmentAcceptedList>
+            <h1>Pending Users</h1>
+            <DataTablePending columns={PendingColumns} data={pendingVolunteers}></DataTablePending>
+            <h1 className='mt-4'>Enrolled Users</h1>
+            <DataTableCompleted columns={EnrolledColumns} data={enrolledVolunteers}></DataTableCompleted>
         </div>
     );
 };
